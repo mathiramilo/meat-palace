0c4b5ee91d27551e9fbb0378104c98866f447a8f
Mathias Ramilo <mathiramilo2290@gmail.com>
2022-05-27 03:41:39 -0300

    Empty Object Funtion

--- src/utils/emptyUser.ts
+++ src/utils/emptyUser.ts
@@ -1,3 +1,3 @@
-// Function that checks if an object is empty (doesent have any propperties)
+// Function that checks if an object is empty (doesnt have any properties)
 
 export const isObjEmpty = (obj: object) => Object.keys(obj).length === 0;
